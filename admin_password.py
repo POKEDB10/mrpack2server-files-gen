6f3bd52f18cf83d6020d01544782f2388ae33549
POKEDB <delta.poke.db@gmail.com>
2025-11-11 22:01:25 +0400

    Updated the Password

--- admin_password.py
+++ admin_password.py
@@ -1,5 +1,5 @@
 import bcrypt
-password = "bruh"
+password = "&zPK!HxcGnEIHt89"
 hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
 print(f"Hashed password: {hashed}")
 
@@ -7,8 +7,8 @@ print(f"Hashed password: {hashed}")
 import bcrypt
 
 username = "pokedb"  # Replace with your username
-password = "WwAaSsDd@1999"  # Replace with your password
-hashed = "$2b$12$3JYMACmzg.434zVSnHwGHuPo10dZPTnB1BGdLCjbmhC9JstD/t2xu"  # Replace with the hash from users.json
+password = "&zPK!HxcGnEIHt89"  # Replace with your password
+hashed = "$2b$12$p3y95mm7gWgQ64jfzXjDCub2jCTYFL7luYEu/2F0jDfy1pWD8Ax5i"  # Replace with the hash from users.json
 if bcrypt.checkpw(password.encode(), hashed.encode()):
     print("Credentials are valid")
 else:
